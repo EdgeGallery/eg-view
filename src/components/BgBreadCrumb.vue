19698b73b8ac74a9705250ad91714525486b69c8
周文敬 <942517636@qq.com>
2021-02-07 17:07:33 +0800

    面包屑组件首次提交

---
+++ src/components/BgBreadCrumb.vue
@@ -0,0 +1,48 @@
+<template>
+  <div
+    class="bread-crumb"
+  >
+    <el-breadcrumb separator=">">
+      <el-breadcrumb-item
+        v-for="item in data"
+        :key="item.name"
+        :to="item.path"
+      >
+        {{ item.name }}
+      </el-breadcrumb-item>
+    </el-breadcrumb>
+  </div>
+</template>
+
+<script>
+
+export default {
+  components: {},
+  data () {
+    return {
+    }
+  },
+  props: {
+    data: {
+      type: Array
+    }
+  },
+  computed: {},
+  watch: {},
+  methods: {
+  },
+  created () {
+  },
+  mounted () {
+  },
+  beforeCreate () {},
+  beforeMount () {},
+  beforeUpdate () {},
+  updated () {},
+  beforeDestroy () {},
+  destroyed () {},
+  activated () {}
+}
+</script>
+<style lang='less' scoped>
+</style>
