53a1c89b64bcafac2e64ec4cd3e8c21a781001d7
周文敬 <942517636@qq.com>
2021-02-23 20:24:11 +0800

    示例优化

---
+++ src/pages/Home.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <eg-banner :image-url="bannerImageUrl"/>
+    <h2/>
+    <el-button
+      type="primary"
+      @click="handleEnterInnerPage"
+    >
+      点击进入内部页面
+    </el-button>
+  </div>
+</template>
+
+<script>
+import EgBanner from '../components/EgBanner'
+export default {
+  components: { EgBanner },
+  data () {
+    return {
+      bannerImageUrl: 'images/静态横幅.png'
+    }
+  },
+  methods: {
+    handleEnterInnerPage () {
+      this.$router.push('/innerPage')
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
---
+++ src/pages/InnerPage.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <eg-bread-crumb :data="crumbData" />
+    <el-button type="primary">点击打开对话框</el-button>
+  </div>
+</template>
+
+<script>
+import EgBreadCrumb from '../components/EgBreadCrumb'
+export default {
+  components: { EgBreadCrumb },
+  data () {
+    return {
+      crumbData: [{
+        name: '首页',
+        path: '/'
+      }, {
+        name: '内部页',
+        path: '/innerPage'
+      }]
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
