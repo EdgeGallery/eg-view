4380ba2e48f5fd38d3e27151245a248d84aba6c5
zoulingli <zll_1208@126.com>
2021-06-23 16:48:08 +0800

    抽取平台底部组件

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 import EgBreadCrumb from './src/components/EgBreadCrumb'
 import EgBanner from './src/components/EgBanner'
 import EgPagination from './src/components/EgPagination'
+import EgFooter from './src/components/EgFooter.vue'
 
 const components = [
   EgBreadCrumb,
   EgBanner,
-  EgPagination
+  EgPagination,
+  EgFooter
 ];
 const install = function(Vue, opts = {}) {
   locale.use(opts.locale);
@@ -23,5 +25,6 @@ if (typeof window !== 'undefined' && window.Vue) {
 export{
   EgBreadCrumb,
   EgBanner,
-  EgPagination
+  EgPagination,
+  EgFooter
 }
